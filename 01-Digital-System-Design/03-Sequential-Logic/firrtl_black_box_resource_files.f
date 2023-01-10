4bd63e6da74bbf3517c461cbec55af527c1e9e93
parkdongho <parkdongho28@naver.com>
2023-01-10 15:08:36 +0900

    UPDATE

--- 01-Digital-System-Design/03-Sequential-Logic/firrtl_black_box_resource_files.f
+++ 01-Digital-System-Design/03-Sequential-Logic/firrtl_black_box_resource_files.f
@@ -1,1 +1,1 @@
-/home/parkdongho/dev/Digital-System-Design-with-Chisel/01-Digital-System-Design/03-Sequential-Logic/BlackBoxSRLC.v
+/home/parkdongho/dev/jupyter-note/tutorial/chisel/Digital-System-Design-with-Chisel/01-Digital-System-Design/03-Sequential-Logic/BlackBoxSRLC.v
